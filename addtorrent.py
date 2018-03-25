941d6a4a8b9d6db95eb11c8328342152c0d3d949
Carlos Millett <caedus75@protonmail.com>
2018-03-25 14:39:00 -0300

    update copyright notice

--- addtorrent.py
+++ addtorrent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env  python3
 
-#Copyright (c) 2016 Carlos Millett
+#Copyright (c) 2016-2018 Carlos Millett
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
